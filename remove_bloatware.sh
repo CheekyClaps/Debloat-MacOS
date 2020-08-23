8ffe5c3f9b2dcd22b428b0c154a5127f1e836b56
CheekyClaps <70092074+CheekyClaps@users.noreply.github.com>
2020-08-23 09:18:51 +0000

    Initial upload

---
+++ remove_bloatware.sh
@@ -0,0 +1,43 @@
+# Remove System apps
+sudo rm -rf /System/Applications/Books.app/
+sudo rm -rf /System/Applications/Calendar.app/
+sudo rm -rf /System/Applications/Chess.app/
+sudo rm -rf /System/Applications/Contacts.app/
+sudo rm -rf /System/Applications/FaceTime.app/
+sudo rm -rf /System/Applications/FindMy.app/
+sudo rm -rf /System/Applications/Home.app/
+sudo rm -rf /System/Applications/Image\ Capture.app/
+sudo rm -rf /System/Applications/Mail.app/
+sudo rm -rf /System/Applications/Maps.app/
+sudo rm -rf /System/Applications/Messages.app/
+sudo rm -rf /System/Applications/Music.app/
+sudo rm -rf /System/Applications/News.app/
+sudo rm -rf /System/Applications/Photo\ Booth.app/
+sudo rm -rf /System/Applications/Photos.app/
+sudo rm -rf /System/Applications/Podcasts.app/
+sudo rm -rf /System/Applications/QuickTime\ Player.app/
+sudo rm -rf /System/Applications/Reminders.app/
+sudo rm -rf /System/Applications/Siri.app/
+sudo rm -rf /System/Applications/Stickies.app/
+sudo rm -rf /System/Applications/Stocks.app/
+sudo rm -rf /System/Applications/TV.app/
+
+# Remove Utillities
+sudo rm -rf /System/Applications/Utilities/Screenshot.app/
+sudo rm -rf /System/Applications/Utilities/Grapher.app/
+sudo rm -rf /System/Applications/Utilities/Script\ Editor.app/
+
+# Remove Applications
+sudo rm -rf /Applications/Safari.app/
+
+# Remove Directorys
+Sudo rm -rf ~/Movies
+Sudo rm -rf ~/Music
+
+# Remove Library components
+Sudo rm -rf ~/Library/iMovie
+Sudo rm -rf ~/Library/Mail
+Sudo rm -rf ~/Library/Safari 
+Sudo rm -rf ~/Library/SafariSafeBrowsing 
+Sudo rm -rf ~/Library/Maps
+Sudo rm -rf ~/Library/Calendars
